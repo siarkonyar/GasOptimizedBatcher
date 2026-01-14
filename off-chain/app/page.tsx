3d9e364403bb0251031fdab5bc6c8b9455c61bf4
siarkonyar <siarkonyar@gmail.com>
2026-01-14 15:08:11 +0000

    fix: delete files related to old batcher script on both hardhat and off-chain next.js app

--- off-chain/app/page.tsx
+++ off-chain/app/page.tsx
@@ -1,4 +1,3 @@
-import ExecuteBatcherButton from "./_components/ExecuteBatcherButton";
 import ExecuteMultiBatcherButton from "./_components/ExecuteMultiBatcherButton";
 import MetaMaskWalletButton from "./_components/MetaMaskWalletButton";
 
@@ -6,7 +5,6 @@ export default function Home() {
   return (
     <div className="space-y-6 p-6">
       <MetaMaskWalletButton />
-      <ExecuteBatcherButton />
       <ExecuteMultiBatcherButton />
     </div>
   );
