efecc9ed85d5acc5b226ecc83f9f4b349c3b396f
siarkonyar <siarkonyar@gmail.com>
2026-01-20 19:01:11 +0000

    fix: add admin wallet to fundWallets.ts script

--- off-chain/scripts/fundWallets.ts
+++ off-chain/scripts/fundWallets.ts
@@ -1,3 +1,4 @@
+import { adminWallet, senders } from "@/lib/keys";
 import { ethers } from "ethers";
 import fs from "fs";
 
@@ -5,27 +6,22 @@ const TENDERLY_RPC =
   "https://virtual.mainnet.eu.rpc.tenderly.co/068e308a-de93-4d7b-8e24-47fe5f7dbeec";
 const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
-const wallets = JSON.parse(fs.readFileSync("lib/wallets.json", "utf8"));
+const senderAddresses = senders.map((s) => s.address) as `0x${string}`[];
+
+const wallets = [adminWallet, ...senderAddresses];
 
 async function fundWallets() {
   const provider = new ethers.JsonRpcProvider(TENDERLY_RPC);
   console.log(`Funding ${wallets.length} wallets`);
 
   for (const wallet of wallets) {
-    /* await provider.send("tenderly_setBalance", [
-      wallet.address,
-      ethers.toQuantity(ethers.parseEther("5")),
-    ]); */ // this funds loop them ethereum but these wallets do not need them since they dont pay the gas
-
     await provider.send("tenderly_setErc20Balance", [
       USDC_ADDRESS,
-      wallet.address,
+      wallet,
       ethers.toQuantity(ethers.parseUnits("500", 6)),
     ]); // puts 500 USDC to each wallet
 
-    console.log(
-      `Funded Wallet #${wallet.id}: ${wallet.address.slice(0, 6)}...`
-    );
+    console.log(`Funded Wallet: ${wallet}`);
   }
 }
 
