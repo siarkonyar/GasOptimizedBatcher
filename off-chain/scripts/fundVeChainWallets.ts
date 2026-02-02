8363f2b689ec15e2c92e345b68e49e1429657ee4
siarkonyar <siarkonyar@gmail.com>
2026-02-02 18:51:58 +0000

    feat: add seed function to fund all vechain wallets with VTHO

--- off-chain/scripts/fundVeChainWallets.ts
+++ off-chain/scripts/fundVeChainWallets.ts
@@ -30,248 +30,6 @@ dotenv.config();
 const THOR_URL = "http://127.0.0.1:8669";
 const USDC_ADDRESS = process.env.NEXT_PUBLIC_VECHAIN_USDC_ADDRESS as string;
 
-const USDC_ABI = [
-  {
-    type: "constructor",
-    inputs: [
-      { name: "name_", type: "string", internalType: "string" },
-      { name: "symbol_", type: "string", internalType: "string" },
-      { name: "decimal_", type: "uint8", internalType: "uint8" },
-    ],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "Approval",
-    type: "event",
-    inputs: [
-      {
-        name: "owner",
-        type: "address",
-        indexed: true,
-        internalType: "address",
-      },
-      {
-        name: "spender",
-        type: "address",
-        indexed: true,
-        internalType: "address",
-      },
-      {
-        name: "value",
-        type: "uint256",
-        indexed: false,
-        internalType: "uint256",
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    name: "OwnershipTransferred",
-    type: "event",
-    inputs: [
-      {
-        name: "previousOwner",
-        type: "address",
-        indexed: true,
-        internalType: "address",
-      },
-      {
-        name: "newOwner",
-        type: "address",
-        indexed: true,
-        internalType: "address",
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    name: "Transfer",
-    type: "event",
-    inputs: [
-      { name: "from", type: "address", indexed: true, internalType: "address" },
-      { name: "to", type: "address", indexed: true, internalType: "address" },
-      {
-        name: "value",
-        type: "uint256",
-        indexed: false,
-        internalType: "uint256",
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    name: "allowance",
-    type: "function",
-    inputs: [
-      { name: "owner", type: "address", internalType: "address" },
-      { name: "spender", type: "address", internalType: "address" },
-    ],
-    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-    stateMutability: "view",
-  },
-  {
-    name: "approve",
-    type: "function",
-    inputs: [
-      { name: "spender", type: "address", internalType: "address" },
-      { name: "amount", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [{ name: "", type: "bool", internalType: "bool" }],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "balanceOf",
-    type: "function",
-    inputs: [{ name: "account", type: "address", internalType: "address" }],
-    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-    stateMutability: "view",
-  },
-  {
-    name: "burn",
-    type: "function",
-    inputs: [{ name: "amount", type: "uint256", internalType: "uint256" }],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "burn",
-    type: "function",
-    inputs: [
-      { name: "account_", type: "address", internalType: "address" },
-      { name: "value_", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "burnFrom",
-    type: "function",
-    inputs: [
-      { name: "account", type: "address", internalType: "address" },
-      { name: "amount", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "decimals",
-    type: "function",
-    inputs: [],
-    outputs: [{ name: "", type: "uint8", internalType: "uint8" }],
-    stateMutability: "view",
-  },
-  {
-    name: "decreaseAllowance",
-    type: "function",
-    inputs: [
-      { name: "spender", type: "address", internalType: "address" },
-      { name: "subtractedValue", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [{ name: "", type: "bool", internalType: "bool" }],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "increaseAllowance",
-    type: "function",
-    inputs: [
-      { name: "spender", type: "address", internalType: "address" },
-      { name: "addedValue", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [{ name: "", type: "bool", internalType: "bool" }],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "mint",
-    type: "function",
-    inputs: [
-      { name: "account_", type: "address", internalType: "address" },
-      { name: "value_", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "name",
-    type: "function",
-    inputs: [],
-    outputs: [{ name: "", type: "string", internalType: "string" }],
-    stateMutability: "view",
-  },
-  {
-    name: "owner",
-    type: "function",
-    inputs: [],
-    outputs: [{ name: "", type: "address", internalType: "address" }],
-    stateMutability: "view",
-  },
-  {
-    name: "renounceOwnership",
-    type: "function",
-    inputs: [],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "symbol",
-    type: "function",
-    inputs: [],
-    outputs: [{ name: "", type: "string", internalType: "string" }],
-    stateMutability: "view",
-  },
-  {
-    name: "totalSupply",
-    type: "function",
-    inputs: [],
-    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-    stateMutability: "view",
-  },
-  {
-    name: "transfer",
-    type: "function",
-    inputs: [
-      { name: "recipient", type: "address", internalType: "address" },
-      { name: "amount", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [{ name: "", type: "bool", internalType: "bool" }],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "transferFrom",
-    type: "function",
-    inputs: [
-      { name: "sender", type: "address", internalType: "address" },
-      { name: "recipient", type: "address", internalType: "address" },
-      { name: "amount", type: "uint256", internalType: "uint256" },
-    ],
-    outputs: [{ name: "", type: "bool", internalType: "bool" }],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "transferOwner",
-    type: "function",
-    inputs: [{ name: "newOwner_", type: "address", internalType: "address" }],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "transferOwnership",
-    type: "function",
-    inputs: [{ name: "newOwner", type: "address", internalType: "address" }],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-  {
-    name: "update",
-    type: "function",
-    inputs: [
-      { name: "name_", type: "string", internalType: "string" },
-      { name: "symbol_", type: "string", internalType: "string" },
-    ],
-    outputs: [],
-    stateMutability: "nonpayable",
-  },
-] as const;
-
 const thorSoloClient = ThorClient.at(THOR_URL, {
   isPollingEnabled: false,
 });
@@ -304,66 +62,46 @@ const provider = new VeChainProvider(
 );
 
 async function seed() {
-  console.log("🚀 Starting Seeding Process (Native SDK Mode)...");
+  console.log("Starting Seeding Process (Native SDK Mode)...");
 
-  const thorClient = ThorClient.at(THOR_URL);
+  const latestBlock = await thorSoloClient.blocks.getBestBlockCompressed();
+  const chainTag = await thorSoloClient.nodes.getChaintag();
 
-  const clauses: TransactionClause[] = [];
+  const clauses: TransactionClause[] = recipients.map(
+    (r) =>
+      Clause.transferVTHOToken(Address.of(r.address), VTHO.of(10000))
+        .clause as TransactionClause,
+  );
 
-  console.log(`📝 Funding ${recipients.length} wallets...`);
+  const gas = HexUInt.of(Transaction.intrinsicGas(clauses).wei).toString();
 
-  // Loop & Fund
-  for (const recipient of recipients) {
-    try {
-      const VETclause = Clause.transferVET(
-        Address.of(recipient.address),
-        VET.of(100),
-      ) as TransactionClause;
-
-      clauses.push(VETclause);
-    } catch (e) {
-      console.error(`❌ Failed to fund ${recipient.address}:`, e);
-      if (e instanceof Error) {
-        console.error(`   Error message: ${e.message}`);
-        if (e.stack) console.error(`   Stack: ${e.stack}`);
-      }
-    }
-  }
-
-  const gasResult = await thorSoloClient.gas.estimateGas(clauses);
-
-  const txBody = await thorSoloClient.transactions.buildTransactionBody(
+  const body: TransactionBody = {
+    chainTag,
+    blockRef: latestBlock !== null ? latestBlock.id.slice(0, 18) : "0x0",
+    expiration: 32,
     clauses,
-    gasResult.totalGas,
-  );
+    gasPriceCoef: 128,
+    gas,
+    dependsOn: null,
+    nonce: 12345678,
+  };
 
-  const signer = await provider.getSigner(senderAccount.address);
+  const signedTransaction = Transaction.of(body).sign(godPrivateKey);
 
-  const rawSignedTransaction = await signer!.signTransaction(
-    signerUtils.transactionBodyToTransactionRequestInput(
-      txBody,
-      senderAccount.address,
-    ),
-  );
+  const encodedRaw = signedTransaction.encoded;
 
-  const signedTransaction = Transaction.decode(
-    HexUInt.of(rawSignedTransaction.slice(2)).bytes,
-    true,
-  );
+  const decodedTx = Transaction.decode(encodedRaw, true);
 
-  // 5 - Send the transaction
   const sendTransactionResult =
-    await thorSoloClient.transactions.sendTransaction(signedTransaction);
+    await thorSoloClient.transactions.sendTransaction(decodedTx);
 
-  // 6 - Wait for transaction receipt
   const txReceipt = await thorSoloClient.transactions.waitForTransaction(
     sendTransactionResult.id,
   );
 
   console.log(txReceipt);
-
-  console.log("🏁 Seeding Complete.");
 }
+
 
 seed().catch((error) => {
   console.error("Fatal error:", error);
