e98762c7757565e17301606686c3764a8430b6d0
siarkonyar <siarkonyar@gmail.com>
2026-02-02 21:28:22 +0000

    feat: add approve for all function

---
+++ off-chain/simulation/VeChainUSDCSimulation.ts
@@ -0,0 +1,111 @@
+import { VECHAIN_USDC_CONTRACT_ABI } from "@/lib/ABI";
+import { recipients } from "@/lib/vechain-wallets";
+import {
+  ABIContract,
+  Address,
+  Clause,
+  Hex,
+  HexUInt,
+  Mnemonic,
+  Secp256k1,
+  Transaction,
+  TransactionBody,
+} from "@vechain/sdk-core";
+import {
+  ProviderInternalBaseWallet,
+  ThorClient,
+  VeChainProvider,
+} from "@vechain/sdk-network";
+import { vechain } from "viem/chains";
+
+const THOR_URL = "http://127.0.0.1:8669";
+const USDC_ADDRESS = process.env.NEXT_PUBLIC_VECHAIN_USDC_ADDRESS as string;
+
+const thorSoloClient = ThorClient.at(THOR_URL, {
+  isPollingEnabled: false,
+});
+
+const godMnemonic =
+  "denial kitchen pet squirrel other broom bar gas better priority spoil cross";
+const godPrivateKey = Mnemonic.toPrivateKey(godMnemonic.split(" "));
+const godPublicKey = Secp256k1.derivePublicKey(godPrivateKey);
+const godAddress = Address.ofPublicKey(godPublicKey).toString();
+
+const senderAccount: { privateKey: string; address: string } = {
+  privateKey: Hex.of(godPrivateKey).toString(),
+  address: godAddress,
+};
+
+const provider = new VeChainProvider(
+  // Thor client used by the provider
+  thorSoloClient,
+
+  // Internal wallet used by the provider (needed to call the getSigner() method)
+  new ProviderInternalBaseWallet([
+    {
+      privateKey: HexUInt.of(senderAccount.privateKey).bytes,
+      address: senderAccount.address,
+    },
+  ]),
+
+  // Disable fee delegation (BY DEFAULT IT IS DISABLED)
+  false,
+);
+
+async function approveSmartContractForAll(provider: VeChainProvider) {
+  console.log("Approving smart contract for all...");
+
+  console.log("This operation is performed only once.");
+
+  try {
+    const latestBlock = await thorSoloClient.blocks.getBestBlockCompressed();
+    const chainTag = await thorSoloClient.nodes.getChaintag();
+
+    const clauses: Clause[] = [
+      ...recipients.map((r) =>
+        Clause.callFunction(
+          Address.of(USDC_ADDRESS),
+          ABIContract.ofAbi(VECHAIN_USDC_CONTRACT_ABI).getFunction("approve"),
+          [r.address, 100000],
+        ),
+      ),
+    ];
+
+    const gas = await thorSoloClient.gas.estimateGas(
+      clauses,
+      senderAccount.address,
+    );
+
+    const body: TransactionBody = {
+      chainTag,
+      blockRef: latestBlock !== null ? latestBlock.id.slice(0, 18) : "0x0",
+      expiration: 32,
+      clauses,
+      gasPriceCoef: 232,
+      gas: gas.totalGas,
+      dependsOn: null,
+      nonce: 12345678,
+    };
+
+    const signedTransaction = Transaction.of(body).sign(godPrivateKey);
+
+    const encodedRaw = signedTransaction.encoded;
+
+    const decodedTx = Transaction.decode(encodedRaw, true);
+
+    const sendTransactionResult =
+      await thorSoloClient.transactions.sendTransaction(decodedTx);
+
+    const txReceipt = await thorSoloClient.transactions.waitForTransaction(
+      sendTransactionResult.id,
+    );
+
+    console.log(txReceipt);
+
+    console.log("✅ All wallets approved the smart contract.");
+    console.log("------------------------------------------------");
+  } catch (error) {
+    console.log(`Error during approval: ${(error as Error).message}`);
+    return false;
+  }
+}
