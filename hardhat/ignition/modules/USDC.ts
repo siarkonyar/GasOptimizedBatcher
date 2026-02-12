e4b7b345a44ccf2869e4f13ca8256d695d727a74
siarkonyar <siarkonyar@gmail.com>
2026-02-12 16:13:58 +0000

    feat: USDC contract address is being saved to .env after deployment

--- hardhat/ignition/modules/USDC.ts
+++ hardhat/ignition/modules/USDC.ts
@@ -1,6 +1,8 @@
 import { ethers } from "hardhat";
 import { stringifyData } from "@vechain/sdk-errors";
 import { USDC_ABI, USDCCreationByteCode } from "../../lib/USDC_Artifact";
+import * as fs from "fs";
+import * as path from "path";
 
 async function main(): Promise<void> {
   const signer = (await ethers.getSigners())[0];
@@ -19,6 +21,36 @@ async function main(): Promise<void> {
     "✅ Contract deployment with the following transaction:",
     stringifyData(txResponse),
   );
+
+  const contractAddress = await txResponse.getAddress();
+
+  saveAddressToEnv(contractAddress);
+}
+
+function saveAddressToEnv(value: string) {
+  const envFilePath = path.join(__dirname, "../../.env");
+  let envContent = "";
+
+  if (fs.existsSync(envFilePath)) {
+    envContent = fs.readFileSync(envFilePath, "utf8");
+  }
+
+  // check if the key already exists
+  const regex = new RegExp(`^USDC_ADDRESS=.*`, "m");
+
+  if (regex.test(envContent)) {
+    // replace existing key
+    envContent = envContent.replace(regex, `USDC_ADDRESS=${value}`);
+    console.log(`Updated USDC_ADDRESS in .env`);
+  } else {
+    // new key
+    const prefix =
+      envContent.length > 0 && !envContent.endsWith("\n") ? "\n" : "";
+    envContent += `${prefix}USDC_ADDRESS=${value}\n`;
+    console.log(`Added USDC_ADDRESS to .env`);
+  }
+
+  fs.writeFileSync(envFilePath, envContent);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
