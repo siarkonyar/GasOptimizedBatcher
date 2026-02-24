7f44ae9031c95d9459a671e17357ce5ea9d140d5
siarkonyar <siarkonyar@gmail.com>
2026-02-24 18:42:26 +0000

    refactor: narrow the random amount creater from 1-100 to 1-3

--- off-chain/lib/randomAmounts.ts
+++ off-chain/lib/randomAmounts.ts
@@ -1,9 +1,8 @@
 import { parseUnits } from "viem";
 
 export const getRandomAmount = () => {
-    // random integer between 1 and 100 USDC
     const min = 1;
-    const max = 100;
+    const max = 3;
     const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
     return parseUnits(randomInt.toString(), 6);
   };
