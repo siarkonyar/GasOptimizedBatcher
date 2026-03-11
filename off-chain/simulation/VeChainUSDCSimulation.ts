64949a4596b7f80da8971d3295f47f9f07a6bd6d
siarkonyar <siarkonyar@gmail.com>
2026-03-11 12:27:06 +0000

    refactor: change 1-U to U in poisson delay functiong

--- off-chain/simulation/VeChainUSDCSimulation.ts
+++ off-chain/simulation/VeChainUSDCSimulation.ts
@@ -82,8 +82,8 @@ function getPoissonDelay(targetTPS: number): number {
 
   const ratePerMs = targetTPS / 1000;
 
-  //The formula: -ln(1-U) / λ
-  const delayMs = -Math.log(1 - Math.random()) / ratePerMs;
+  //The formula: -ln(U) / λ
+  const delayMs = -Math.log(Math.random()) / ratePerMs;
 
   return delayMs;
 }
